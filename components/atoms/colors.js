ea508fe730851a729ed234f5925ef012d50ed300
Andres Bastidas <andbastidasfierro@gmail.com>
2022-04-26 22:43:49 -0500

    initial layout ready

---
+++ components/atoms/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+    appbarColor : '#b0bbf8',
+    avatarBg: '#3c8ca9',
+    footerColor: '#666',
+    blueBright: '#3f51b5',
+    blueDark: '#1f222c',
+    bluePale: '#2a2e3d',
+    textLight: '#4a4d65',
+    secondary: '#eb7171',
+    white: '#ffffff',
+    lightGray: '#d6dce6'
+}
+
+module.exports = colors;
---
+++ components/templates/Layout.tsx
@@ -0,0 +1,108 @@
+import React from "react";
+
+import {Theme} from "@mui/system";
+import {AppBar, CssBaseline, Typography, Container} from "@mui/material";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import colors from "../atoms/colors";
+
+import Image from "next/image";
+import Head from 'next/head';
+
+type Props = {
+    children?: React.ReactNode;
+}
+
+
+const theme : Theme = createTheme({
+    palette: {
+        //type: 'dark',
+        primary: {
+            main: colors.blueBright,
+        },
+        secondary: {
+            main: colors.secondary,
+        },
+        background: {
+            default: colors.blueDark,
+            paper: colors.bluePale,
+        },
+        text: {
+            primary: colors.white,
+            secondary: colors.white,
+        },
+    },
+});
+
+
+const styles = {
+
+    appbar: {
+        padding: '8px',
+        display: 'flex',
+        fontSize: '0.8em',
+        userSelect: 'none' as 'none',
+    },
+    content: {
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    footer: {
+        textAlign: 'center' as 'center',
+        zIndex: -1,
+        bottom: 0,
+        width: '100%',
+        fontSize: '0.6em',
+        display: 'block',
+        marginTop: 100,
+        color: colors.footerColor,
+    },
+}
+
+const title : string = 'pokitdex by abf';
+
+const Layout: React.FC<Props> = ( { children } ) => {
+
+    return (<div>
+
+        <Head>
+            <title>{title}</title>
+            <meta name="description" content="Andres Bastidas F - Media Monks Challenge" />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <AppBar>
+                    <div style={styles.appbar}>
+                        <Image src="/vercel.svg" alt={title} width={40} height={16}/>
+                        <Typography component="div" sx={{ flexGrow: 1, fontWeight: 800 }}>
+                            { title }
+                        </Typography>
+
+                    </div>
+                </AppBar>
+                <Container component="main" sx={styles.content}>
+                    {children}
+                </Container>
+            </ThemeProvider>
+        </main>
+
+        <footer>
+            <a
+                href="https://github.com/andna/pokitdex"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={styles.footer}
+            >
+                By Andres Bastidas Fierro @ 2022
+            </a>
+        </footer>
+    </div>)
+}
+
+export default Layout;
+
