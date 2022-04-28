0d3c291e8cacaf1bec596c6d422f86e599d6bb45
Andres Bastidas <andbastidasfierro@gmail.com>
2022-04-28 13:49:56 -0300

    typing colors

--- components/atoms/colors.js
+++ components/atoms/colors.js
@@ -8,7 +8,27 @@ const colors = {
     textLight: '#4a4d65',
     secondary: '#eb7171',
     white: '#ffffff',
-    lightGray: '#d6dce6'
+    lightGray: '#d6dce6',
+    types: {
+        normal: "#a8a878",
+        fire: "#f08030",
+        water: "#6890f0",
+        electric: "#f8d030",
+        grass: "#78c850",
+        ice: "#98d8d8",
+        fighting: "#c03028",
+        poison: "#a040a0",
+        ground: "#e0c068",
+        flying: "#a890f0",
+        psychic: "#f85888",
+        bug: "#a8b820",
+        rock: "#b8a038",
+        ghost: "#705898",
+        dragon: "#7038f8",
+        dark: "#705848",
+        steel: "#b8b8d0",
+        fairy: "#ee99ac"
+    }
 }
 
 module.exports = colors;
--- components/molecules/Typing.tsx
+++ components/molecules/Typing.tsx
@@ -3,14 +3,26 @@ import {Type} from "../../types/Pokemon";
 import {
     Chip
 } from "@mui/material";
+import colors from "../atoms/colors";
 
-
+const styles = {
+    chip: {
+        textTransform: 'uppercase',
+        marginRight: 1,
+        fontSize: 10,
+        fontFamily: 'system-ui',
+        letterSpacing: '-0.3px',
+        fontWeight: 800
+    }
+}
 type Props = {
     typing: Type;
 }
 
 const Typing: React.FC<Props> = ({ typing }) => {
-    return <Chip label={typing.type.name} />
+    return <Chip
+                sx={{...styles.chip, background: colors.types[typing.type.name] }}
+                label={typing.type.name} />
 }
 
 export default Typing;
